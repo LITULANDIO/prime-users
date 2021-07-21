c1f4f47b69b8300e0c8caf560b25fc93e4949934
litus <litulandio@gmail.com>
2021-07-21 20:52:25 +0200

    initial project

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,16 @@
+<template src="./Home.html"></template>
+
+<script>
+export default {
+    name: 'Home',
+    setup(){
+
+        return{
+            
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" src="./Home.scss" scoped></style>
