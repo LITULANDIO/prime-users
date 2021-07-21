87c2812aa52b0d0e81f651a355106fd1cdcc94fb
litus <litulandio@gmail.com>
2021-07-21 22:41:03 +0200

    assembly cmp table in Home

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
+<div class="py-5 px-5">
   <router-view/>
+</div>
 </template>
 
 <style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,12 +1,32 @@
 <template src="./Home.html"></template>
 
 <script>
+import Table from "@/components/Table/Table.vue";
+import { ref, onBeforeMount } from '@vue/runtime-core';
+
 export default {
     name: 'Home',
+    components:{
+        Table,
+    },
     setup(){
 
+        const headersRefList = ref(null);
+        const rowsRefList = ref(null);
+
+        onBeforeMount(() =>{
+            getHeaders();
+
+        });
+
+        const getHeaders = () =>{
+            headersRefList.value = ['Género', 'Nombre', 'Nacionalidad', 'Fecha de Nacimiento', 'Fecha de registro'];
+        }
+
+
         return{
-            
+            headersRefList,
+            rowsRefList
         }
     }
 
