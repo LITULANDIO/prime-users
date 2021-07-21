2884bccb2cfa4d20907c01a7c1e64fda86b0c234
litus <litulandio@gmail.com>
2021-07-21 22:40:26 +0200

    create cmp table users

---
+++ src/components/Table/Table.vue
@@ -0,0 +1,27 @@
+<template src="./Table.html"></template>
+
+<script>
+export default {
+    name: 'Table',
+    props:{
+        headers: {
+            type: Array,
+            required: true
+    },
+        rows: {
+            type: Array,
+            required: true
+    },
+
+    },
+    setup(){
+
+        return{
+
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" src="./Table.scss" scoped></style>
