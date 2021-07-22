fbefe1c21c671e6a7624a1a5a37970f201377bb1
litus <litulandio@gmail.com>
2021-07-22 22:06:38 +0200

    install & config gogmaps

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import './registerServiceWorker';
 import router from './router';
 import './assets/tailwindcss/main.css';
 import store from './store/index.js';
+import VueGoogleMaps from '@fawmi/vue-google-maps';
 
 const app = createApp(App)
 
 app.use(router)
-app.use.apply(store)
+app.use(store)
+app.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyAJtxFwGzPtJ4YjuCn6OvzkzzgPK-DBUZg',
+    },
+})
 app.mount('#app')
