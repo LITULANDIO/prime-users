af62b7e88541051896402c26e974ca172368ae0c
litus <litulandio@gmail.com>
2021-07-22 22:09:22 +0200

    new getter & setter in state for vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,38 @@
 import { createStore } from 'vuex';
 import createPersistedState from "vuex-persistedstate";
-
+import { removeItemFromArr } from "@/utils/utils.js";
 
 export default createStore({
   state: {
-      param: null
+      param: null,
+      users: [],
+      favorites: []
   },
   mutations: {
-      SET_PARAM(state, param){
+    SET_PARAM(state, param){
           state.param = param;
-      }
+      },
+    SET_USERS(state, users){
+        state.users = users;
+    },
+    SET_LIKE_FAVORITES(state, favorites){
+        state.favorites.push(favorites)
+    },
+    SET_DONT_LIKE_FAVORITES(state, favorites){
+        removeItemFromArr(state.favorites, favorites)
+    }
   },
   actions: {
   },
   getters: {
-      GET_PARAM(state){
+    GET_PARAM(state){
           return state.param;
+      },
+    GET_USERS(state){
+          return state.users
+      },
+    GET_FAVORITES(state){
+          return state.favorites
       }
   },
   plugins: [createPersistedState()],
