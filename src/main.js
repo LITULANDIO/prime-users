9a310f0467520b98e756b8a3d42989e7e7dd71a3
litus <litulandio@gmail.com>
2021-07-22 17:13:15 +0200

    install vuex & persistate date

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import './assets/tailwindcss/main.css';
-
+import store from './store/index.js';
 
 const app = createApp(App)
 
 app.use(router)
+app.use.apply(store)
 app.mount('#app')
