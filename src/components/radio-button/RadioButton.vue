7c9887aa0b9d6de81ba9d2381e79c9da566dbb67
litus <litulandio@gmail.com>
2021-07-23 01:57:18 +0200

    create cmp radioButton

---
+++ src/components/radio-button/RadioButton.vue
@@ -0,0 +1,24 @@
+<template src="./RadioButton.html"></template>
+
+<script>
+export default {
+    name: 'RadioButton',
+    props:{
+        name:{
+            type: String,
+        },
+        label:{
+            type: String,
+            required: true
+        },
+        value:{
+            type: [String, Boolean, Number]
+        },
+
+    },
+    emits:['selected-radio', 'update:checked'],
+    
+}
+</script>
+
+<style lang="scss" src="./RadioButton.scss" scoped></style>
