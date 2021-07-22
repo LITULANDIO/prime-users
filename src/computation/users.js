8fac3e05f96ccf9cf9d90edaa2b62ca4941d0e7c
litus <litulandio@gmail.com>
2021-07-22 22:08:33 +0200

    refactors Home & computation-data

--- src/computation/users.js
+++ src/computation/users.js
@@ -35,3 +35,23 @@ export const getDataUsers = async() =>{
 
     return newUsersList;
 }
+
+export const getDataUserDetail = async({id}) =>{
+
+    const usersList = await fetchUsers();
+    let newUsersObj = {};
+
+    if(usersList){
+        usersList.results.forEach((user, index) => {
+            if(index == id){
+                console.log(user)
+                newUsersObj = {
+                    personal_data: { img: user.picture.large, name: user.name.first, email: user.email, phone: user.phone },
+                    coordinates: { lat: parseInt(user.location.coordinates.latitude), long: parseInt(user.location.coordinates.longitude) },
+                    location_data: { city: user.location.city, street: user.location.street.name, num: user.location.street.number }
+                }
+            }
+        });
+    }
+    return newUsersObj;
+}
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -5,6 +5,8 @@ import { getDataUsers } from "@/computation/users.js";
 
 import Table from "@/components/Table/Table.vue";
 import { ref, onBeforeMount } from '@vue/runtime-core';
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
 export default {
     name: 'Home',
@@ -12,28 +14,50 @@ export default {
         Table,
     },
     setup(){
-
+        const store = useStore();
+        const router = useRouter();
         const headersRefList = ref(null);
         const rowsRefList = ref(null);
+        const dataDetail = ref(null);
 
         onBeforeMount(() =>{
             getHeaders();
-            getRows();
-
-        });
+            getExistRows();
+        });       
 
         const getHeaders = () =>{
             headersRefList.value = ['Género', 'Nombre', 'Email', 'Nacionalidad', 'Fecha de Nacimiento', 'Fecha de registro'];
         }
 
         const getRows = async () =>{
-            rowsRefList.value = await getDataUsers();
+            store.commit('SET_USERS', await getDataUsers())
+            rowsRefList.value = store.getters["GET_USERS"]
+            console.log(store)
+        }
+
+        const getExistRows = () =>{
+            if(store.getters["GET_USERS"] != []){
+                rowsRefList.value = store.getters["GET_USERS"] 
+            }
+        }
+
+       
+        const onGoDetail = (id) =>{
+            store.commit('SET_PARAM', id.id);
+            router.push(`/detail-user/${id.id}`)
+        }
+
+        const onShowUsers = () =>{
+            getRows();
         }
 
 
         return{
             headersRefList,
-            rowsRefList
+            rowsRefList,
+            dataDetail,
+            onGoDetail,
+            onShowUsers
         }
     }
 
